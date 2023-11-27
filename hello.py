ef21449ebca50b243c9ebe1584028f3ae6165751
urunmatsoma <henry.daniel1273@gmail.com>
2023-11-26 22:33:32 -0500

    first pyton code

---
+++ hello.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n==0:
+        return :
+     else:
+         return n * factorial(n -1)
+         
+number =5
+result =factorial(number)
+print(f"The factoriL of (number) is (result).")
+
+        
